6d3f05a23e4318de529b92335d51452a0ffb4303
mamoko <mamokosf@gmail.com>
2019-09-26 17:32:52 +0300

    working on the task

---
+++ components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Link } from 'react-router';
+
+const Header = () => (
+  <header class=".navbar bg-dark">
+    <a class="navbar-brand" ><Link to='/'>GifSearch</Link></a>
+  </header>
+)
---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom'
+import { BrowserRouter } from 'react-router'
+import App from './components/App';
+
+render((
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
+), document.getElementById('root'));
