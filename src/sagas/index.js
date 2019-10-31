09a72a1ee468a22e4e16655879acb515b67369f8
mamoko <mamokosf@gmail.com>
2019-10-31 17:40:14 +0300

    phase 4 is reworked

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -7,7 +7,8 @@ import {
 } from 'redux-saga/effects';
 
 import { gifsLoaded, gifInfoLoaded } from '../actions';
-import { getGifs, getGif } from '../services/api';
+import { getGifs, getGif, createApiRequest } from '../services/api';
+
 
 function* fetchGifList(action) {
   const offset = action.payload;
@@ -20,6 +21,21 @@ function* fetchSingleGif(action) {
   yield put(gifInfoLoaded({ gif: response, isFetching: false }));
 }
 
+function deleteGif(action) {
+  const request = createApiRequest('DELETE', 'application/json', JSON.stringify(action.payload));
+  console.log(request);
+}
+
+function addGif(action) {
+  const request = createApiRequest('POST', 'multipart/form-data', action.payload);
+  console.log(request);
+}
+
+function editGif(action) {
+  const request = createApiRequest('POST', 'application/json', JSON.stringify(action.payload));
+  console.log(request);
+}
+
 function* watchFetchGifList() {
   yield takeLatest('FETCH_GIF_LIST', fetchGifList);
 }
@@ -28,11 +44,26 @@ function* watchFetchSingleGif() {
   yield takeLatest('FETCH_SINGLE_GIF', fetchSingleGif);
 }
 
+function* watchDeleteGifRequest() {
+  yield takeLatest('DELETING_SUBMITTED', deleteGif);
+}
+
+function* watchAddGifRequest() {
+  yield takeLatest('ADDING_SUBMITTED', addGif);
+}
+
+function* watchEditGifRequest() {
+  yield takeLatest('EDITING_SUBMITTED', editGif);
+}
+
 export default function* rootSaga() {
   yield all(
     [
       fork(watchFetchGifList),
       fork(watchFetchSingleGif),
+      fork(watchDeleteGifRequest),
+      fork(watchAddGifRequest),
+      fork(watchEditGifRequest),
     ],
   );
 }
